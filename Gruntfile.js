56ec1f98cc820e06603b697c1cd43adf314c84f7
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-01-28 18:52:34 +0200

    transition to Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+/*jshint node:true */
+module.exports = function( grunt ) {
+
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-closure-compiler' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	function process( code ) {
+		return code
+			// Embed version
+			.replace( /@VERSION/g, grunt.config( 'pkg' ).version );
+	}
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( 'package.json' ),
+		concat: {
+			options: { process: process },
+			'default': {
+				src: [
+					'src/DGTable.js',
+					'src/DGTable.ColumnCollection.js',
+					'src/DGTable.RowCollection.js'
+				],
+				dest: 'dist/DGTable.js'
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			'src-js': {
+				src: [ 'src/*.js' ]
+			},
+			'dist': {
+				src: [ 'dist/*.js' ]
+			}
+		},
+		'closure-compiler': {
+			"dist": {
+				js: 'dist/DGTable.js',
+				jsOutputFile: 'dist/DGTable.min.js',
+				
+				noreport: true,
+				maxBuffer: 500,
+				closurePath: 'closure-compiler',
+				options: {
+					compilation_level: 'ADVANCED_OPTIMIZATIONS',
+					language_in: 'ECMASCRIPT5_STRICT',
+					externs: [
+						'closure-compiler/externs/jquery-1.9.externs',
+						'closure-compiler/externs/underscore-1.5.2.externs',
+						'closure-compiler/externs/backbone-1.1.0.externs'
+					],
+                    create_source_map: 'dist/DGTable.min.js.map',
+                    source_map_format: 'V3'
+				}
+			}
+		},
+		'add-map-directive': {
+			"dist": {
+				paths: 'dist/DGTable.min.js'
+			}
+		},
+		watch: {
+			files: [ '*', '.jshintrc', '{src}/**/{*,.*}' ],
+			tasks: 'default'
+		}
+	});
+    
+    grunt.registerMultiTask('add-map-directive', 'Append source-map directive to output file', function() {
+        var paths = grunt.file.expand(this.data.paths);
+        paths.forEach(function(path) {
+            grunt.file.write(path, 
+                grunt.file.read(path) + '//# sourceMappingURL=DGTable.min.js.map');
+        });
+    });
+
+	grunt.registerTask( 'build', [ 'jshint:src-js', 'concat', 'closure-compiler', 'add-map-directive' ] );
+	grunt.registerTask( 'style', [ 'jshint:src-js' ] );
+	grunt.registerTask( 'default', [ 'build' ] );
+
+};
